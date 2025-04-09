2e5f720412616b626a90285d23fa708ff821dcde
gracekarinn <gracekarina12@gmail.com>
2025-04-09 12:51:48 +0700

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "udehnih-review-rating"
---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/UdehnihReviewRatingApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihreviewrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdehnihReviewRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdehnihReviewRatingApplication.class, args);
+    }
+
+}
