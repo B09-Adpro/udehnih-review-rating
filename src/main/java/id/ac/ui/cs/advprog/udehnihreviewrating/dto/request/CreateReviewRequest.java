fb39d2a683c5117e6b059389d540c046a038f9e0
gracekarinn <gracekarina12@gmail.com>
2025-04-10 17:33:33 +0700

    feat: add create review request dto

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/dto/request/CreateReviewRequest.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.udehnihreviewrating.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateReviewRequest {
+    private String courseId;
+    private String reviewText;
+    private int rating;
+    private boolean anonymous;
+}
