215f22312dbf9db3bde8a7ecb183d83e10dcca4b
gracekarinn <gracekarina12@gmail.com>
2025-04-10 17:11:13 +0700

    fix: correct the decorator based on unit test

--- src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/model/Review.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/model/Review.java
@@ -3,10 +3,7 @@ package id.ac.ui.cs.advprog.udehnihreviewrating.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,8 +14,10 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Review {
     @Id
+    @EqualsAndHashCode.Include
     private UUID id;
     private String courseId;
     private String studentId;
