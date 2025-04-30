886b7c2170302caec49e198c87675f8a216d2ecf
gracekarinn <gracekarina12@gmail.com>
2025-04-30 15:11:42 +0700

    feat: add student details test

---
+++ src/test/java/id/ac/ui/cs/advprog/udehnihreviewrating/security/StudentDetailsTest.java
@@ -0,0 +1,71 @@
+package id.ac.ui.cs.advprog.udehnihreviewrating.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentDetailsTest {
+
+    private StudentDetails studentDetails;
+    private final String testId = "test@example.com";
+    private final String testEmail = "test@example.com";
+    private final List<SimpleGrantedAuthority> authorities =
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_STUDENT"));
+
+    @BeforeEach
+    void setUp() {
+        studentDetails = new StudentDetails(testId, testEmail, authorities);
+    }
+
+    @Test
+    void testGetId() {
+        assertEquals(testId, studentDetails.getId());
+    }
+
+    @Test
+    void testGetEmail() {
+        assertEquals(testEmail, studentDetails.getEmail());
+    }
+
+    @Test
+    void testGetUsername() {
+        assertEquals(testEmail, studentDetails.getUsername());
+    }
+
+    @Test
+    void testGetPassword() {
+        assertNull(studentDetails.getPassword());
+    }
+
+    @Test
+    void testGetAuthorities() {
+        assertEquals(authorities, studentDetails.getAuthorities());
+        assertTrue(studentDetails.getAuthorities().iterator().next().getAuthority().equals("ROLE_STUDENT"));
+    }
+
+    @Test
+    void testIsAccountNonExpired() {
+        assertTrue(studentDetails.isAccountNonExpired());
+    }
+
+    @Test
+    void testIsAccountNonLocked() {
+        assertTrue(studentDetails.isAccountNonLocked());
+    }
+
+    @Test
+    void testIsCredentialsNonExpired() {
+        assertTrue(studentDetails.isCredentialsNonExpired());
+    }
+
+    @Test
+    void testIsEnabled() {
+        assertTrue(studentDetails.isEnabled());
+    }
+}
