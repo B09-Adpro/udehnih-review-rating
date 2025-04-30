f85c07b9ed2840582a222e319bf74f9cdab28fc6
gracekarinn <gracekarina12@gmail.com>
2025-04-30 15:11:46 +0700

    feat: add student details

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/security/StudentDetails.java
@@ -0,0 +1,50 @@
+package id.ac.ui.cs.advprog.udehnihreviewrating.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+public class StudentDetails implements UserDetails {
+    private String id;
+    private String email;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public StudentDetails(String id, String email, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
