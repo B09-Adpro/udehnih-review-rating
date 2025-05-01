01f3915899dfe903031098dc823d51ba5391e69a
gracekarinn <gracekarina12@gmail.com>
2025-05-01 21:30:05 +0700

    feat: add course detail dto

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/dto/course/CourseDetailDTO.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.udehnihreviewrating.dto.course;
+
+import java.math.BigDecimal;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseDetailDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String tutorName;
+    private BigDecimal price;
+}
