1451b2650965634236a695c6f9b3f41ea60bb6d6
gracekarinn <gracekarina12@gmail.com>
2025-05-01 21:25:38 +0700

    feat: add new property test

--- src/test/java/id/ac/ui/cs/advprog/udehnihreviewrating/UdehnihReviewRatingApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/udehnihreviewrating/UdehnihReviewRatingApplicationTests.java
@@ -3,14 +3,17 @@ package id.ac.ui.cs.advprog.udehnihreviewrating;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.cloud.compatibility-verifier.enabled=false"
+})
 class UdehnihReviewRatingApplicationTests {
 
     @Test
     void contextLoads() {
 
     }
-
 }
