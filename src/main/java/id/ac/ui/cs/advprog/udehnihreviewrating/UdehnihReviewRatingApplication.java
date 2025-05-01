57083b01952694fe15644c01e67a5faed2f65e75
gracekarinn <gracekarina12@gmail.com>
2025-05-01 21:25:48 +0700

    feat: add new property

--- src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/UdehnihReviewRatingApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/UdehnihReviewRatingApplication.java
@@ -2,12 +2,12 @@ package id.ac.ui.cs.advprog.udehnihreviewrating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UdehnihReviewRatingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UdehnihReviewRatingApplication.class, args);
     }
-
 }
