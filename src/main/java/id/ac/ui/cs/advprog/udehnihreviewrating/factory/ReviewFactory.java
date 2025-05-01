b7eafa47a563ce5ad1fd59d2605dbcced52d4b9a
gracekarinn <gracekarina12@gmail.com>
2025-05-01 21:32:11 +0700

    fix: change to long

--- src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/factory/ReviewFactory.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/factory/ReviewFactory.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.udehnihreviewrating.factory;
 import id.ac.ui.cs.advprog.udehnihreviewrating.model.Review;
 
 public interface ReviewFactory {
-    Review createBasicReview(String courseId, String studentId, String reviewText, int rating);
-    Review createRatingOnlyReview(String courseId, String studentId, int rating);
-    Review createDetailedReview(String courseId, String studentId, String reviewText, int rating, boolean isAnonymous);
+    Review createBasicReview(Long courseId, String studentId, String reviewText, int rating);
+    Review createRatingOnlyReview(Long courseId, String studentId, int rating);
+    Review createDetailedReview(Long courseId, String studentId, String reviewText, int rating, boolean isAnonymous);
 }
