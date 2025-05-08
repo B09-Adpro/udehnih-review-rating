cb6927883bc8151bcb913e37f6ffa1eb4fe356fe
gracekarinn <gracekarina12@gmail.com>
2025-05-09 00:30:24 +0700

    fix: url environment

--- src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/client/CourseClient.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/client/CourseClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "course-service", url = "${course-service.url}")
+@FeignClient(name = "course-service", url = "${COURSE_SERVICE_URL}")
 public interface CourseClient {
 
     @GetMapping("/api/courses/{courseId}")
