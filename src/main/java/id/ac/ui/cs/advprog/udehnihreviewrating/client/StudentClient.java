7df7f8601506bf49410beeb548b4eadeb399140d
gracekarinn <gracekarina12@gmail.com>
2025-05-08 21:05:55 +0700

    feat: add student client

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/client/StudentClient.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.udehnihreviewrating.client;
+
+import id.ac.ui.cs.advprog.udehnihreviewrating.dto.student.StudentDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "auth-service", url = "${AUTH_SERVICE_URL}")
+public interface StudentClient {
+    @GetMapping("/api/users/{studentId}")
+    StudentDTO getStudentById(@PathVariable("studentId") String studentId);
+}
