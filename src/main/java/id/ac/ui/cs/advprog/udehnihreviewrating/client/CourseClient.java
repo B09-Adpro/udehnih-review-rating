ead5f8edae56130265806e5c8143545ec259c836
gracekarinn <gracekarina12@gmail.com>
2025-05-16 09:47:53 +0700

    feat: add feign config to course client

--- src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/client/CourseClient.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/client/CourseClient.java
@@ -1,11 +1,16 @@
 package id.ac.ui.cs.advprog.udehnihreviewrating.client;
 
+import id.ac.ui.cs.advprog.udehnihreviewrating.config.FeignConfig;
 import id.ac.ui.cs.advprog.udehnihreviewrating.dto.course.CourseDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "course-service", url = "${course-service.url}")
+@FeignClient(
+        name = "course-service",
+        url = "${course-service.url}",
+        configuration = FeignConfig.class
+)
 public interface CourseClient {
 
     @GetMapping("/api/courses/{courseId}")
