cde392e000eaa864d21b57916eebb8c4f65c98e6
gracekarinn <gracekarina12@gmail.com>
2025-05-16 09:48:56 +0700

    fix: change to long data type

--- src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/client/StudentClient.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihreviewrating/client/StudentClient.java
@@ -1,12 +1,13 @@
 package id.ac.ui.cs.advprog.udehnihreviewrating.client;
 
+import id.ac.ui.cs.advprog.udehnihreviewrating.config.FeignConfig;
 import id.ac.ui.cs.advprog.udehnihreviewrating.dto.student.StudentDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-service", url = "${auth-service.url}")
+@FeignClient(name = "auth-service", url = "${auth-service.url}", configuration = FeignConfig.class)
 public interface StudentClient {
     @GetMapping("/api/users/{studentId}")
-    StudentDTO getStudentById(@PathVariable("studentId") String studentId);
+    StudentDTO getStudentById(@PathVariable("studentId") Long studentId);
 }
